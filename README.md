cc64197bc43cc04d5345cd864b96896b77424b12
SBY <307823745@qq.com>
2022-06-04 19:38:45 +0800

    ps0

---
+++ README.md
@@ -0,0 +1,3 @@
+# MIT 6.005: Software Construction
+
+[6.005: Software Construction (mit.edu)](https://ocw.mit.edu/ans7870/6/6.005/s16/)
--- ps0/ps0/src/rules/RulesOf6005.java
+++ ps0/ps0/src/rules/RulesOf6005.java
@@ -35,10 +35,9 @@ public class RulesOf6005 {
     public static boolean mayUseCodeInAssignment(boolean writtenByYourself,
             boolean availableToOthers, boolean writtenAsCourseWork,
             boolean citingYourSource, boolean implementationRequired) {
-        
-        // TODO: Fill in this method, then remove the exception
-        
-        throw new RuntimeException("implement me!");
+
+        return writtenByYourself || availableToOthers || writtenAsCourseWork ||
+                citingYourSource || implementationRequired;
     }
     
     /**
